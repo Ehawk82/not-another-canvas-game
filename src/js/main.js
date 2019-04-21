7efd3ba8c0de74d002ea7a81a9e8f4d0aeee5178
Jason Graziano <mrehawk82@gmail.com>
2019-04-21 13:42:56 -0400

    initial commits

---
+++ src/js/main.js
@@ -0,0 +1,68 @@
+var myUI;
+
+myUI = {
+	init: function(){
+		var browser = function() {
+    // Return cached result if avalible, else get result then cache it.
+    if (browser.prototype._cachedResult)
+        return browser.prototype._cachedResult;
+
+    // Opera 8.0+
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+
+    // Chrome 1+
+    var isChrome = !!window.chrome;
+
+    // Blink engine detection
+    var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+    return browser.prototype._cachedResult =
+        isOpera ? 'Opera' :
+        isFirefox ? 'Firefox' :
+        isSafari ? 'Safari' :
+        isChrome ? 'Chrome' :
+        isIE ? 'IE' :
+        isEdge ? 'Edge' :
+        isBlink ? 'Blink' :
+        "Don't know";
+
+	};
+myUI.loader(browser);
+
+		
+	},
+	loader: function(browser){
+		var btnHolder = createEle("div"),
+            browser = browser();
+
+		btnHolder.innerHTML = "designed on " + browser;
+
+
+if (browser === "Chrome") {
+	demo.append(btnHolder);
+}
+if (browser === "IE") {
+	demo.appendChild(btnHolder);
+}
+console.log(browser);
+
+
+		
+	}
+};
+
+window.onload = function() {
+    myUI.init();
+};
