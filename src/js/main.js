18d5fef52afc15070fca55b60452470bbaa4c720
Jason Graziano <mrehawk82@gmail.com>
2019-04-22 12:11:28 -0400

    catching up with myself

--- src/js/main.js
+++ src/js/main.js
@@ -2,64 +2,45 @@ var myUI;
 
 myUI = {
 	init: function(){
-		var browser = function() {
-    // Return cached result if avalible, else get result then cache it.
-    if (browser.prototype._cachedResult)
-        return browser.prototype._cachedResult;
-
-    // Opera 8.0+
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-    // Firefox 1.0+
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-
-    // Safari 3.0+ "[object HTMLElementConstructor]" 
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
-
-    // Internet Explorer 6-11
-    var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-    // Edge 20+
-    var isEdge = !isIE && !!window.StyleMedia;
-
-    // Chrome 1+
-    var isChrome = !!window.chrome;
-
-    // Blink engine detection
-    var isBlink = (isChrome || isOpera) && !!window.CSS;
-
-    return browser.prototype._cachedResult =
-        isOpera ? 'Opera' :
-        isFirefox ? 'Firefox' :
-        isSafari ? 'Safari' :
-        isChrome ? 'Chrome' :
-        isIE ? 'IE' :
-        isEdge ? 'Edge' :
-        isBlink ? 'Blink' :
-        "Don't know";
-
-	};
-myUI.loader(browser);
-
-		
+		const cH = window.innerHeight, cW = window.innerWidth;
+		    console.log(cH);
+		myUI.loader();
 	},
-	loader: function(browser){
+	loader: function(){
+		myUI.makeCanvas();
 		var btnHolder = createEle("div"),
-            browser = browser();
+		    startBtn = createEle("button");
 
-		btnHolder.innerHTML = "designed on " + browser;
+		    startBtn.innerHTML = "START";
+		    startBtn.className = "menu_buttons";
+		    startBtn.onclick = myUI.startGame();
 
+			btnHolder.className = "btnHolder";
+			btnHolder.appendChild(startBtn);
 
-if (browser === "Chrome") {
-	demo.append(btnHolder);
-}
-if (browser === "IE") {
-	demo.appendChild(btnHolder);
-}
-console.log(browser);
+			demo.appendChild(btnHolder);
+	},
+	makeCanvas: function() {
+		var canvas = createEle("canvas");
 
+		canvas.height = 420;
+		canvas.width = 680;
 
-		
+	    demo.appendChild(canvas);
+	},
+	startGame: function() {
+		return function() {
+			var canvas = byTag("canvas", 0),
+		        ctx = canvas.getContext("2d");
+			myUI.runTheThing(canvas, ctx);
+		}
+	},
+	runTheThing: function(canvas, ctx) {
+		if (canvas) {
+	//cycle
+    	} else {
+			console.log("canvas is null or not available");
+    	}
 	}
 };
 
