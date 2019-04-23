f951ebfa9a1281e340fc19ac04ac6bb0e766bac5
Jason Graziano <mrehawk82@gmail.com>
2019-04-23 13:03:32 -0400

    back to the drawing board

--- src/js/main.js
+++ src/js/main.js
@@ -3,45 +3,13 @@ var myUI;
 myUI = {
 	init: function(){
 		const cH = window.innerHeight, cW = window.innerWidth;
-		    console.log(cH);
-		myUI.loader();
+		
+		myUI.loader(cH,cW);
 	},
-	loader: function(){
-		myUI.makeCanvas();
-		var btnHolder = createEle("div"),
-		    startBtn = createEle("button");
+	loader: function(cH,cW){
 
-		    startBtn.innerHTML = "START";
-		    startBtn.className = "menu_buttons";
-		    startBtn.onclick = myUI.startGame();
-
-			btnHolder.className = "btnHolder";
-			btnHolder.appendChild(startBtn);
-
-			demo.appendChild(btnHolder);
 	},
-	makeCanvas: function() {
-		var canvas = createEle("canvas");
 
-		canvas.height = 420;
-		canvas.width = 680;
-
-	    demo.appendChild(canvas);
-	},
-	startGame: function() {
-		return function() {
-			var canvas = byTag("canvas", 0),
-		        ctx = canvas.getContext("2d");
-			myUI.runTheThing(canvas, ctx);
-		}
-	},
-	runTheThing: function(canvas, ctx) {
-		if (canvas) {
-	//cycle
-    	} else {
-			console.log("canvas is null or not available");
-    	}
-	}
 };
 
 window.onload = function() {
