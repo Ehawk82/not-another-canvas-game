cfe5d65da58069349f91c4d32565d5764b58dd08
Jason Graziano <mrehawk82@gmail.com>
2019-04-23 15:12:47 -0400

    new stuff

---
+++ src/js/lib.js
@@ -0,0 +1,81 @@
+var btnNames = ["▫","▪","◽","◾","◻","◼","⬜","⬛","🔲","🔳"];
+
+var userNACG = {
+	level: 1,
+	quark: {
+		"up": {},
+		"down": {},
+	},
+	particle: {
+		"proton": {},
+		"nuetron": {},
+		"electron": {}
+	},
+	element: {
+		"H": {},
+		"O": {},
+		"C": {}
+	},
+	compound: {
+		"water": {},
+		"air": {},
+	},
+	mass: {
+		"land": {},
+		"sea": {}
+	},
+    organism: {
+    	"singleCell": {},
+    	"multiCell": {},
+    	"aquatic": {},
+        "bacterium": {},
+        "fungi": {},
+        "fauna": {},
+        "floral": {}
+    },
+    beings: {
+    	"floresiensis": {},
+    	"habilis": {},
+        "rudolfensis": {},
+        "erectus": {},
+        "ergaster": {},
+        "erectus_s_s": {},
+        "denisovan": {},
+        "antecessor": {},
+        "heidelbergensis": {},
+        "heidelbergensis_s_s": {},
+        "neanderthalensis": {},
+        "denisova": {},
+        "sapiens": {},
+        "illustra": {}
+    },
+    tech: {
+    	"stone": {},
+    	"iron": {},
+    	"bronze": {},
+    	"dark": {},
+    	"industrial": {},
+    	"atomic": {},
+    	"post_atomic": {},
+    	"nano": {},
+    	"post_nano": {},
+    },
+    spaceship: {
+    	"alpha": {},
+    	"beta": {},
+    	"theta": {},
+    	"epsilon": {},
+    	"omega": {},
+    	"zeta": {}
+    },
+    wormholeKey: {
+    	"sagA": {},
+    	"sagB": {},
+    	"metA": {},
+    	"metB": {},
+    	"metC": {},
+    	"jazA": {},
+    	"jazB": {}
+    },
+    
+};
--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,30 @@ var myUI;
 myUI = {
 	init: function(){
 		const cH = window.innerHeight, cW = window.innerWidth;
-		
+        LSinit("userNACG", userNACG);
 		myUI.loader(cH,cW);
 	},
 	loader: function(cH,cW){
+		var btnHolder = createEle("div");
 
+        for (var b=0;b<10;b++) {
+        	var btns = createEle("button");
+
+        	btns.innerHTML = btnNames[b];
+        	btns.className = "btns";
+
+        	btns.disabled = true;
+
+			btnHolder.appendChild(btns);
+        }
+
+		btnHolder.className = "btnHolder";
+		setTimeout(function() {
+			makeFull(btnHolder);
+		}, 200);
+
+
+		demo.appendChild(btnHolder);
 	},
 
 };
